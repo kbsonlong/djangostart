d0d1818c350d72a0bd2db0bbfbe8bb2845074b73
kbsonlong <13610018065@139.com>
2017-06-20 18:19:59 +0800

    ss

--- apps/message/views.py
+++ apps/message/views.py
@@ -3,8 +3,25 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from models import UserMessage
 # Create your views here.
 
 def getform(request):
+    # all_messages = UserMessage.objects.all()
+    # for message in all_messages:
+    #     print message.name
+
+    if request.method == "POST":
+        name = request.POST.get('name','')
+        message = request.POST.get('message','')
+        address = request.POST.get('address','')
+        email = request.POST.get('email','')
+        user_message = UserMessage()
+        user_message.name = name
+        user_message.message = message
+        user_message.address = address
+        user_message.email = email
+        user_message.object_id = "3"
+        user_message.save()
 
     return render(request, "message_form.html")
